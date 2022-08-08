af5c50227157cacb60fb76be5e23044b79e82beb
Damian Canteros <dellacortedamian@gmail.com>
2022-08-08 19:34:30 -0300

    hasta el ejercicio 9

--- Ejercicio_8/Ejercicio_8.java
+++ Ejercicio_8/Ejercicio_8.java
@@ -23,17 +23,72 @@ ingresa el usuario y devuelve verdadero si la contiene y falso si no.
  */
 package Ejercicio_8;
 
+import Ejercicio_8.Entidad.Cadena;
+import Ejercicio_8.Servicio.CadenaServicio;
+import java.util.Scanner;
+
 /**
  *
  * @author Damian
  */
 public class Ejercicio_8 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner leer = new Scanner(System.in);
+        CadenaServicio s1 = new CadenaServicio();
+        
+        Cadena c1 = s1.crearCadena();
+        int opcion;
+        String frase="";
+        String letra="";
+        
+        do {
+            System.out.println("");  
+            System.out.println("elija una opcion");
+            System.out.println("1 contabilizar la cantidad de vocales que tiene la frase");
+            System.out.println("2 invertir la frase ingresada y mostrarla por pantalla");
+            System.out.println("Ingresar otra frase y:");
+            System.out.println("3 comparar la longitud de las frases");
+            System.out.println("4 unir las frases");
+            System.out.println("Ingresar un caracter y:");
+            System.out.println("5 contabilizar cuántas veces se repite el carácter en la frase");
+            System.out.println("6 reemplazar todas las letras “a” por el carácter ingresado");
+            System.out.println("7 comprobar si la frase contiene el carácter ingresado");
+            System.out.println("8 salir");
+            opcion = leer.nextInt();
+
+            if (opcion==3 || opcion==4) {
+                System.out.println("ingrese la nueva frase");
+                frase = leer.next();
+            }else if (opcion==5 || opcion==6 || opcion==7) {
+                System.out.println("ingrese el caracter");
+                letra = leer.next();
+            }
+
+            switch(opcion){
+                case 1:
+                    System.out.println("la frase tiene "+s1.mostrarVocales(c1)+" vocales") ;
+                break;
+                case 2:
+                    s1.invertirFrase(c1);
+                break;
+                case 3:
+                    System.out.println(s1.compararLongitud(frase, c1));
+                break;
+                case 4:
+                    s1.unirFrases(frase, c1);
+                break;
+                case 5:
+                    s1.vecesRepetido(letra, c1);
+                break;
+                case 6:
+                    s1.reemplazar(letra, c1);
+                break;
+                case 7:
+                   System.out.println(s1.contiene(letra, c1));
+                break;
+            }   
+        } while (opcion !=8);           
     }
-    
 }
